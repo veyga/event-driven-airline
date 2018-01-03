2e296378b2241c1237d35c21809af099e0bac62c
Andrew Stefanich <andrewstefanich@gmail.com>
2018-01-02 21:09:15 -0800

    lombok getter

--- common/src/main/java/io/astefanich/airline/common/commands/AddPassengerToFlightCommand.java
+++ common/src/main/java/io/astefanich/airline/common/commands/AddPassengerToFlightCommand.java
@@ -3,8 +3,10 @@ package io.astefanich.airline.common.commands;
 import io.astefanich.airline.common.domain.BookingNumber;
 import io.astefanich.airline.common.domain.FlightNumber;
 import io.astefanich.airline.common.domain.PassengerName;
+import lombok.Getter;
 import org.axonframework.commandhandling.TargetAggregateIdentifier;
 
+@Getter
 public class AddPassengerToFlightCommand {
 
   private BookingNumber bookingNumber;
@@ -21,15 +23,4 @@ public class AddPassengerToFlightCommand {
     this.passenger = passenger;
   }
 
-  public BookingNumber getBookingNumber() {
-    return bookingNumber;
-  }
-
-  public FlightNumber getFlightNumber() {
-    return flightNumber;
-  }
-
-  public PassengerName getPassenger() {
-    return passenger;
-  }
 }
